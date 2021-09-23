282290cce32e0cd89722c81765b08443941d944b
Usman <m.usman@qbatch.com>
2021-09-23 16:46:48 +0500

    backend separate

---
+++ Backend/src/routers/products.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = new express.Router();
+
+const ProductsStore = require('../models/products');
+
+router.post("/products", async(req,res) => {
+    try {
+        const addProducts = new ProductsStore(req.body);
+        console.log(req.body);
+        let insertProducts = await addProducts.save();
+        res.status(201).send(insertProducts);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+router.get("/products", async(req,res) => {
+    try {
+        const getProducts = await ProductsStore.find({});
+        res.send(getProducts);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+})
+
+module.exports = router;
