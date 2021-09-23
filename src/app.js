0660946580670cb1141ec225ea23385fa96c1b75
Usman <m.usman@qbatch.com>
2021-09-23 16:19:46 +0500

    product api created

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
-
-const ProductsStore = require('../src/models/products');
+// const bodyParser = require('body-parser')
 require("../src/db/conn")
+const ProductsStore = require('../src/models/products');
+const router = require('./routers/products')
 
 const port = process.env.PORT || 3000;
 
-app.post("/products", async(req,res) => {
-    try {
-        const addProducts = new ProductsStore(req.body);
-        console.log(req.body);
-        addProducts.save();
-    } catch (error) {
-        res.send(error);
-    }
-})
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`connection is live at port : ${port}`);
