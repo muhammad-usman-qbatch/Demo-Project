b7252f3e32ffaf25084a0e910117c0570d53d07f
Usman <m.usman@qbatch.com>
2021-09-24 19:15:23 +0500

    .

--- Frontend/src/components/Products.js
+++ Frontend/src/components/Products.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getProductsList, getCount } from '../../reducers/getProducts';
+import { getProductsList} from '../../reducers/getProducts';
 
 export default function Products() {
 
