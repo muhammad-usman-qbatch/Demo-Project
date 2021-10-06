cc98f867998aca3cfc0bf664dd90e19af65aceed
Usman <m.usman@qbatch.com>
2021-10-06 17:20:17 +0500

    Cart Backend done based on User Id.

--- Backend/src/routes/store.js
+++ Backend/src/routes/store.js
@@ -4,12 +4,13 @@ import { gettingFromCart } from '../controllers/store/cart/getFromCart';
 import { gettingProducts } from '../controllers/store/product/getProducts';
 import { addingProducts } from '../controllers/store/product/addProducts';
 import { getProductDetail } from '../controllers/store/product/getProductDetail';
+import { requireLogin } from '../middlewares/auth';
 
 const Router = express.Router();
 
 // cart related requests
-Router.post('/addToCart', AddingToCart);
-Router.get('/cart', gettingFromCart);
+Router.post('/addToCart',requireLogin, AddingToCart);
+Router.get('/cart',requireLogin, gettingFromCart);
 
 // product related requests
 Router.post('/products', addingProducts);
