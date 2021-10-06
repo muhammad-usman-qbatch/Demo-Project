56a28073f35a27d3f5e5d6ddd67e8689b68b9f06
Usman <m.usman@qbatch.com>
2021-10-06 19:20:07 +0500

    Cart Frontend on the basis of user id done

--- Backend/src/middlewares/auth.js
+++ Backend/src/middlewares/auth.js
@@ -1,17 +1,18 @@
 import jwt from 'jsonwebtoken';
 
 export const requireLogin = async(req,res,next) => {
-    const token = req.headers;
-    console.log('token', token.authorization);
+    const token = req.headers.authorization.split(" ")[1];
+    // const token = req.headers.authorization;
+    console.log('token-1', token);
     if (!token){
         res.status(403).json({error:"Token is required for authentication."});
     }
     try {
-        const decoded = jwt.verify(token.authorization, process.env.TOKEN_KEY);
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
         req.user = decoded;
         console.log('req.user', req.user);
         next();
     } catch (error) {
-        res.status(401).json({error:"Invalid Token."});
+        res.status(500).json({error:"Invalid Token."});
     }
 }
